658fa0e39085aad93fc27f3bce03db3125066ea6
Andrej Snitko <snitko@seznam.cz>
2017-06-20 00:56:05 +0200

    Welcome page added

---
+++ app/presenters/WelcomePresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+
+class WelcomePresenter extends BasePresenter {
+
+    public function __construct() {
+    }
+
+    public function renderDefault() {
+    }
+}
